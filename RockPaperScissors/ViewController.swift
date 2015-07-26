e3bd074548107dff8f91bf38068e65f0f39f3bcd
kacperbranicki <kacper.branicki@gmail.com>
2015-07-26 09:17:53 +0200

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Kacper Branicki on 26.07.2015.
+//  Copyright (c) 2015 Kacper Branicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
