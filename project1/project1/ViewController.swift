c48997312cc8f6fd01fd94942b2f0ffc887969f9
appcamp <appcamp@systemss-MacBook-Pro-6.local>
2015-08-15 10:04:24 +0200

    przypomnienie dzialania komunikacji iphon -> watch

---
+++ project1/project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by appcamp on 8/15/15.
+//  Copyright (c) 2015 appcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
